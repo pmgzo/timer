c07a65642bb7d9482bec9760fa06015794258f19
pmgzo <pierremickael.gonzalo@gmail.com>
2024-12-02 11:22:55 +0100

    feat(timer): update timer based on current time and not based on setInterval

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,18 @@ const formatTimeNumber = (value: number): string => {
   return `${Math.floor(value)}`.padStart(2, "0")
 }
 
+interface TimerState {
+  activated: boolean;
+  timestamp: number;
+  time: number// in millisecs 
+}
+
 export default function Home() {
 
   const [minuteTimer, setMinuteTimer] = useState<number>(0);
   const [secondTimer, setSecondTimer] = useState<number>(0);
 
-  const [timerActivated, setTimerActivated] = useState<boolean>(false);
+  const [timer, setTimer] = useState<TimerState>({activated: false, timestamp: 0, time: 0});
 
 
   function changeSecondTimer({target: {value}}: ChangeEvent<HTMLInputElement>) {
@@ -35,20 +41,27 @@ export default function Home() {
   }
 
   useEffect(() => {
-    if (timerActivated && (secondTimer > 0 || minuteTimer > 0)) {
-      const interval = setInterval(() => {
-        if (secondTimer === 0) {
-          setSecondTimer(59)
-          if (minuteTimer > 0) {
-            setMinuteTimer(minuteTimer - 1)
-          }
-        } else {
-          setSecondTimer(secondTimer - 1)
-        }
-      }, 1000);
-      return () => clearInterval(interval)
+
+    const now = Date.now()
+    if (timer.activated) {
+      const ellapsedTime = now - timer.timestamp
+
+      if (ellapsedTime >= timer.time) {
+        setTimer({activated: false, timestamp: 0, time: 0})
+      }
+      else {
+        const interval =  setInterval(() => {
+          const now2 = Date.now()
+          const ellapsedTime2 = now2 - timer.timestamp
+
+          setMinuteTimer(Math.trunc((timer.time - ellapsedTime2) / 1000 / 60))
+          setSecondTimer(Math.trunc((timer.time - ellapsedTime2) / 1000 % 60))
+        }, 1000);
+
+        return () => clearInterval(interval)
+      }
     }
-  }, [timerActivated, minuteTimer, secondTimer])
+  }, [timer, minuteTimer, secondTimer])
 
   return (
     <div>
@@ -59,9 +72,16 @@ export default function Home() {
         <input name="minute-input" value={minuteTimer} onChange={changeMinuteTimer}/>
         <input name="second-input" value={secondTimer} onChange={changeSecondTimer}/>
         <button className="bg-green-400" onClick={() => {
-          setTimerActivated(true)
+          setTimer({activated: true, timestamp: Date.now(), time: minuteTimer * 60 * 1000 + secondTimer * 1000})
         }
       }>Start</button>
+        <button className="bg-red-400" onClick={() => {
+          setTimer({activated: false, timestamp: 0, time: 0})
+          setMinuteTimer(0)
+          setSecondTimer(0)
+        }
+      }>Reset</button>
+      
       </div>
     </div>
   );
